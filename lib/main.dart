4f8167eac5e7cfc5bba21dc6ed9ded91d165222a
MrFarhan <muhammadfarhan012345@gmail.com>
2022-05-08 19:39:44 +0500

    created custom class and added author name in quote

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import "quote.dart";
 
 void main() => runApp(MaterialApp(home: Quotes()));
 
@@ -11,13 +12,22 @@ class Quotes extends StatefulWidget {
 }
 
 class _QuotesState extends State<Quotes> {
-  List<String> quotes = [
-    'Any fool can write code that a computer can understand. ...',
-    'First, solve the problem. ...',
-    'Experience is the name everyone gives to their mistakes. ',
-    ' In order to be irreplaceable, one must always be different” - Coco Chanel ',
-    'Java is to JavaScript what car is to Carpet.” ',
-    'Knowledge is power.'
+
+  List<Quote> quotes = [
+    Quote(
+        author: "unknown",
+        text: "Any fool can write code that a computer can understand."),
+    Quote(author: "unknown", text: "First, solve the problem."),
+    Quote(
+        author: "unknown",
+        text: "Experience is the name everyone gives to their mistakes. "),
+    Quote(
+        author: "Coco Chanel",
+        text: "In order to be irreplaceable, one must always be different"),
+    Quote(
+        author: "unknown",
+        text: "Java is to JavaScript what car is to Carpet."),
+    Quote(author: "unknown", text: "Knowledge is power."),
   ];
   @override
   Widget build(BuildContext context) {
@@ -31,7 +41,9 @@ class _QuotesState extends State<Quotes> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: quotes.map((item) => Text(item)).toList(),
+            children: quotes
+                .map((quote) => Text('${quote.text} ' '-' ' ${quote.author}'))
+                .toList(),
           ),
         ));
   }
