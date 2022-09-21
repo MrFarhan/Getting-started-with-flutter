2a80466b0ba4e27b7c0fd56a29eb811258378da0
MrFarhan <muhammadfarhan012345@gmail.com>
2022-09-22 02:02:41 +0500

    Updated singleton stream to sink with new notes too, and added list view for notes list and fixed typo on creating table / constants

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -13,11 +13,15 @@ class NotesService {
   List<DatabaseNote> _notes = [];
 
   static final NotesService _shared = NotesService._sharedInstance();
-  NotesService._sharedInstance();
+  NotesService._sharedInstance() {
+    _notesStreamController =
+        StreamController<List<DatabaseNote>>.broadcast(onListen: () {
+      _notesStreamController.sink.add(_notes);
+    });
+  }
   factory NotesService() => _shared;
 
-  final _notesStreamController =
-      StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
@@ -303,7 +307,7 @@ const idColumn = 'id';
 const emailColumn = 'email';
 const userIdColumn = 'user_id';
 const textColumn = 'text';
-const isSyncedWithCloudColumn = 'is_syncyed_with_cloud';
+const isSyncedWithCloudColumn = 'is_synced_with_cloud';
 const createUserTable =
     '''CREATE TABLE IF NOT EXISTS "user"("id" INTEGER NOT NULL, "email" TEXT NOT NULL UNIQUE, PRIMARY KEY ("id" AUTOINCREMENT));''';
 const createNoteTable = '''CREATE TABLE IF NOT EXISTS "note" (
