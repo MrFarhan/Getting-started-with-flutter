66e8829fecc9afef9e43f3dbf4a1af3ea63a3463
MrFarhan <muhammadfarhan012345@gmail.com>
2022-09-27 00:32:12 +0500

    Created firebase CRUD backend methods and also added exceptions

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -98,7 +98,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == "foo@bar.com") throw UserNotFoundAuthException();
     if (password == "foobarbaz") throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: 'foo@bar.com');
+    const user = AuthUser(
+      isEmailVerified: false,
+      email: 'foo@bar.com',
+      id: 'myid',
+    );
     _user = user;
     return Future.value(user);
   }
@@ -116,7 +120,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: 'foo@bar.com');
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: 'foo@bar.com',
+      id: 'myid',
+    );
     _user = newUser;
   }
 }
