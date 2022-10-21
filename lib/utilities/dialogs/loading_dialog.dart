a00fbbb661896e936acbd22ab4b3dbbe0ccb0118
MrFarhan <muhammadfarhan012345@gmail.com>
2022-10-21 21:24:21 +0500

    Added auth email verification

---
+++ lib/utilities/dialogs/loading_dialog.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+typedef CloseDialog = void Function();
+
+CloseDialog showLoadingDialog({
+  required String text,
+  required BuildContext context,
+}) {
+  final dialog = AlertDialog(
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CircularProgressIndicator(),
+        SizedBox(height: 10.0),
+        Text(text),
+      ],
+    ),
+  );
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) => dialog,
+  );
+  return () => Navigator.of(context).pop();
+}
